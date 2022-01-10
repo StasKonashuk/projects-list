0ba00196649c919edec5d315c9e3b67bec75d35b
Stanislav <skonashuk@list.ru>
2022-01-10 15:02:48 +0300

    Create edit and create routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
+import { CreatePage } from './CreatePage/CreatePage';
+import { EditPage } from './EditPage/EditPage';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/projects-list" element={<Main />} />
+      <Route path="/create" element={<CreatePage />} />
+      <Route path="/edit" element={<EditPage />} />
+    </Routes>
   );
-}
+};
+
+const Main = () => {
+  return <div>Projects</div>;
+};
 
 export default App;
