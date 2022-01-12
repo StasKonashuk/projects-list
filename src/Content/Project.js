5bd681e8a96bdc7ee93e4af794b7b93c7a45089e
Stanislav <skonashuk@list.ru>
2022-01-12 19:55:06 +0300

    Create Project component

---
+++ src/Content/Project.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import style from './Content.module.css';
+
+const Project = () => {
+  return (
+    <div className={style.projectContainer}>
+      <h1>Name of project</h1>
+      <p>Project title</p>
+      <p>Project text</p>
+      <p>Project author</p>
+      <p>Author offer</p>
+      <p>Language of project</p>
+      <p>Name of company</p>
+      <p>email</p>
+      <p>Soauthors</p>
+      <p>Number of soathors</p>
+      <p>Surname soathors</p>
+      <p>Task status</p>
+      <p>Task name</p>
+    </div>
+  );
+};
+
+export default Project;
