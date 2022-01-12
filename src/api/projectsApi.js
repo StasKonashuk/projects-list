064534b30ec5db2f9dc85ae169d061755b7e1b4a
Stanislav <skonashuk@list.ru>
2022-01-12 20:34:01 +0300

    Create projectsApi

---
+++ src/api/projectsApi.js
@@ -0,0 +1,71 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const springApi = createApi({
+  reducerPath: 'projectsApi',
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BACKEND_URL }),
+  endpoints: builder => ({
+    getProjects: builder.query({
+      query: () => ({
+        url: '/api/projects'
+      })
+    }),
+    addProject: builder.mutation({
+      query: ({
+        id,
+        companyName,
+        email,
+        projectAuthor,
+        projectDescripton,
+        projectLanguage,
+        projectName,
+        projectTasks,
+        projectTitle,
+        projectVersionSystemControl
+      }) => ({
+        url: '/api/create-project',
+        method: 'post',
+        body: {
+          id,
+          companyName,
+          email,
+          projectAuthor,
+          projectDescripton,
+          projectLanguage,
+          projectName,
+          projectTasks,
+          projectTitle,
+          projectVersionSystemControl
+        }
+      })
+    }),
+    editProject: builder.mutation({
+      query: ({
+        id,
+        companyName,
+        email,
+        projectAuthor,
+        projectDescripton,
+        projectLanguage,
+        projectName,
+        projectTasks,
+        projectTitle,
+        projectVersionSystemControl
+      }) => ({
+        url: '/api/update-project',
+        method: 'put',
+        body: {
+          id,
+          companyName,
+          email,
+          projectAuthor,
+          projectDescripton,
+          projectLanguage,
+          projectName,
+          projectTasks,
+          projectTitle,
+          projectVersionSystemControl
+        }
+      })
+    })
+  })
+});
