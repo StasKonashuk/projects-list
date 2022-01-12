8465591cca3c596455c31f536bfa6dce9407922e
Stanislav <skonashuk@list.ru>
2022-01-12 23:50:34 +0300

    Add Table to Content component

--- src/Content/Content.js
+++ src/Content/Content.js
@@ -1,36 +1,15 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import Project from './Project';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 import style from './Content.module.css';
-import { useGetProjectsQuery } from '../api/projectsApi';
-import { getProjects } from '../redux/projects/projects-reducer';
+import Table from './Table';
 
 const Content = () => {
-  const dispaatch = useDispatch();
-
-  const { data, isSuccess } = useGetProjectsQuery();
-
-  useEffect(async () => {
-    try {
-      if (isSuccess) {
-        const projects = await data;
-        console.log(data);
-        dispaatch(getProjects({ projects }));
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  });
-
-  const projects = useSelector(state => state.projects.projects);
-
   return (
     <div className={style.projectsContainer}>
-      <div className={style.projectstList}>
-        <Project />
-        <Project />
-        <Project />
-      </div>
+      <Table />
+      <NavLink className={style.createProjectButton} to="/create">
+        Create Project
+      </NavLink>
     </div>
   );
 };
