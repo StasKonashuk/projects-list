3c1bca00f5075268dd8c26b067df55e3d8f80d95
Stanislav <skonashuk@list.ru>
2022-01-14 20:48:22 +0300

    Add validation schema to forms

--- src/CreatePage/CreatePage.js
+++ src/CreatePage/CreatePage.js
@@ -2,12 +2,12 @@ import React, { useEffect } from 'react';
 import { Form, Formik, Field, FieldArray } from 'formik';
 import { NavLink } from 'react-router-dom';
 import { TextField, Select } from 'formik-mui';
-import { object, string, array } from 'yup';
 import { useDispatch, useSelector } from 'react-redux';
+import { schema } from '../validation/schema';
 import { useAddProjectMutation } from '../api/projectsApi';
 import {
   createProject,
-  refreshCreateProjectMessage
+  refreshCreateProjectMessage,
 } from '../redux/projects/projects-reducer';
 import style from './CreatePage.module.css';
 
@@ -20,7 +20,7 @@ const CreatePage = () => {
     dispatch(refreshCreateProjectMessage());
   }, []);
 
-  const succesMsg = useSelector(state => state.projects.createProjectMessage);
+  const succesMsg = useSelector((state) => state.projects.createProjectMessage);
 
   return (
     <div className={style.createFormContainer}>
@@ -47,52 +47,13 @@ const CreatePage = () => {
             projectTasks: [
               {
                 name: '',
-                status: ''
-              }
+                status: '',
+              },
             ],
             projectLanguage: { status: '', language: '' },
-            projectVersionSystemControl: ''
+            projectVersionSystemControl: '',
           }}
-          validationSchema={object({
-            projectName: string()
-              .required('You need to provide Project Name')
-              .min(4, 'Project Name need to be at least 4 characters')
-              .max(25, 'Project Name is too long'),
-            projectTitle: string()
-              .required('You need to provide Project Title')
-              .min(6, 'Project Title need to be at least 6 characters')
-              .max(25, 'Project Title is too long'),
-            projectDescripton: string()
-              .required('You need to provide Project Description')
-              .min(1, 'Project Description need to be at least 1 character')
-              .max(140, 'Project Description is too long'),
-            projectAuthor: string()
-              .required('You need to provide Project Author')
-              .min(4, 'Project Author need to be at least 4 characters')
-              .max(25, 'Project Author is too long'),
-            projectVersionSystemControl: string().required(
-              'You need to provide Version Control System'
-            ),
-            email: string('Email must be a string'),
-            companyName: string('Company name must be a string'),
-            projectLanguage: object({
-              language: string().required(
-                'You need to provide Project language'
-              )
-            }),
-            projectTasks: array(
-              object({
-                name: string()
-                  .required('Task Name is needed')
-                  .min(3, 'Task Name needs to be at least 3 character')
-                  .max(20, 'Task Name is too long'),
-                status: string()
-                  .required('Task Status is needed')
-                  .min(3, 'Task Status needs to be at least 3 character')
-                  .max(12, 'Task Status is too long')
-              })
-            )
-          })}
+          validationSchema={schema}
           onSubmit={async (values, { setSubmitting }) => {
             try {
               const data = await addProject(values);
--- src/EditPage/EditPage.js
+++ src/EditPage/EditPage.js
@@ -2,13 +2,13 @@ import React, { useEffect } from 'react';
 import { Form, Formik, Field, FieldArray } from 'formik';
 import { NavLink } from 'react-router-dom';
 import { TextField } from 'formik-mui';
-import { object, string, array } from 'yup';
 import { useDispatch, useSelector } from 'react-redux';
+import { schema } from '../validation/schema';
 import { useEditProjectMutation } from '../api/projectsApi';
 import {
   editProject,
   refreshEditProjectMessage,
-  removeProjectId
+  removeProjectId,
 } from '../redux/projects/projects-reducer';
 import style from './EditPage.module.css';
 
@@ -21,11 +21,11 @@ const EditPage = () => {
     dispatch(refreshEditProjectMessage());
   }, [dispatch]);
 
-  const succesMsg = useSelector(state => state.projects.editProjectMessage);
+  const successMsg = useSelector((state) => state.projects.editProjectMessage);
 
-  const project = useSelector(state =>
+  const project = useSelector((state) =>
     state.projects.projects.filter(
-      p => p.project_id === state.projects.projectId
+      (p) => p.project_id === state.projects.projectId
     )
   );
 
@@ -34,9 +34,9 @@ const EditPage = () => {
       <div className={style.titleEditForm}>
         <h1>Edit project</h1>
       </div>
-      {succesMsg ? (
+      {successMsg ? (
         <div className={style.successEditProjectMessage}>
-          <div>{succesMsg}</div>
+          <div>{successMsg}</div>
           <NavLink className={style.backToProjectsButton} to="/projects-list">
             Back to projects
           </NavLink>
@@ -51,58 +51,19 @@ const EditPage = () => {
             projectAuthor: project[0].project_author,
             email: project[0].email,
             companyName: project[0].company_name,
-            projectTasks: project[0].tasks.map(t => ({
+            projectTasks: project[0].tasks.map((t) => ({
               id: t.task_id,
               name: t.task_name,
-              status: t.task_status
+              status: t.task_status,
             })),
             projectLanguage: {
               status: project[0].project_language_status,
-              language: project[0].project_language
+              language: project[0].project_language,
             },
             projectVersionSystemControl:
-              project[0].project_version_system_control
+              project[0].project_version_system_control,
           }}
-          validationSchema={object({
-            projectName: string()
-              .required('You need to provide Project Name')
-              .min(4, 'Project Name need to be at least 4 characters')
-              .max(25, 'Project Name is too long'),
-            projectTitle: string()
-              .required('You need to provide Project Title')
-              .min(6, 'Project Title need to be at least 6 characters')
-              .max(25, 'Project Title is too long'),
-            projectDescripton: string()
-              .required('You need to provide Project Description')
-              .min(1, 'Project Description need to be at least 1 character')
-              .max(140, 'Project Description is too long'),
-            projectAuthor: string()
-              .required('You need to provide Project Author')
-              .min(4, 'Project Author need to be at least 4 characters')
-              .max(25, 'Project Author is too long'),
-            projectVersionSystemControl: string().required(
-              'You need to provide Version Control System'
-            ),
-            email: string('Email must be a string'),
-            companyName: string('Company name must be a string'),
-            projectLanguage: object({
-              language: string().required(
-                'You need to provide Project language'
-              )
-            }),
-            projectTasks: array(
-              object({
-                name: string()
-                  .required('Task Name is needed')
-                  .min(3, 'Task Name needs to be at least 3 character')
-                  .max(20, 'Task Name is too long'),
-                status: string()
-                  .required('Task Status is needed')
-                  .min(3, 'Task Status needs to be at least 3 character')
-                  .max(12, 'Task Status is too long')
-              })
-            )
-          })}
+          validationSchema={schema}
           onSubmit={async (values, { setSubmitting }) => {
             try {
               const data = await updateProject(values);
